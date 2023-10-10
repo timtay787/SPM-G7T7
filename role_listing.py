e06511d0fe3a2b5f8dea2c48fd8e9b61af93b0ba
Timothy Tay <timothy.tay.2021@scis.smu.edu.sg>
2023-10-11 02:18:25 +0800

    set up post and put functions for role listings

--- role_listing.py
+++ role_listing.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
 from flask_cors import CORS
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -15,27 +17,27 @@ class Role_Listing(db.Model):
     Role_Listing_ID = db.Column(db.Integer, primary_key= True)
     Role_ID = db.Column(db.Integer, nullable=False)
     Role_Listing_Desc = db.Column(db.String(50000), nullable=False)
-    Role_Listing_Source = db.Column(db.Integer, primary_key= True)
+    Role_Listing_Source = db.Column(db.Integer, nullable=False)
     Role_Listing_Open = db.Column(db.Date, nullable=False)
     Role_Listing_Close = db.Column(db.Date, nullable=False)
     Role_Listing_Creator = db.Column(db.Integer, nullable=False)
     Role_Listing_ts_create = db.Column(db.DateTime, nullable=False)
-    Country = db.Column(db.String(50), nullable=False)
-    Career_Level = db.Column(db.String(50), nullable=False)
+    # Country = db.Column(db.String(50), nullable=False)
+    # Career_Level = db.Column(db.String(50), nullable=False)
     Role_Listing_Updater = db.Column(db.Integer, nullable=False)
     Role_Listing_ts_update = db.Column(db.DateTime, nullable=False)
 
-    def __init__(self, Role_Listing_ID, Role_ID, Role_Listing_Desc, Role_Listing_Source, Role_Listing_Open, Role_Listing_Close, Role_Listing_Creator, Role_Listing_ts_create, Country, Career_Level, Role_Listing_Updater, Role_Listing_ts_update):
+    def __init__(self, Role_Listing_ID, Role_ID, Role_Listing_Desc, Role_Listing_Source, Role_Listing_Open, Role_Listing_Close, Role_Listing_Creator, Role_Listing_ts_create, Role_Listing_Updater, Role_Listing_ts_update):
         self.Role_Listing_ID = Role_Listing_ID
         self.Role_ID = Role_ID
-        self.Role_Listing_Description = Role_Listing_Desc
+        self.Role_Listing_Desc = Role_Listing_Desc
         self.Role_Listing_Source = Role_Listing_Source
         self.Role_Listing_Open = Role_Listing_Open
         self.Role_Listing_Close = Role_Listing_Close
         self.Role_Listing_Creator = Role_Listing_Creator
         self.Role_Listing_ts_create = Role_Listing_ts_create
-        self.Country = Country
-        self.Career_Level = Career_Level
+        # self.Country = Country
+        # self.Career_Level = Career_Level
         self.Role_Listing_Updater = Role_Listing_Updater
         self.Role_Listing_ts_update = Role_Listing_ts_update
 
@@ -48,8 +50,8 @@ class Role_Listing(db.Model):
                 "RoleListingClose": self.Role_Listing_Close,
                 "RoleListingCreator": self.Role_Listing_Creator,
                 "RoleListingTimestampCreate": self.Role_Listing_ts_create,
-                "Country": self.Country,
-                "CareerLevel": self.Career_Level,
+                # "Country": self.Country,
+                # "CareerLevel": self.Career_Level,
                 "RoleListingUpdater": self.Role_Listing_Updater,
                 "RoleListingTimestampUpdate": self.Role_Listing_ts_update
                 }
@@ -67,6 +69,107 @@ class Candidates(db.Model):
         return {"CandidateID": self.Candidate_ID,
                 "RoleListingID": self.Role_Listing_ID
                 }
+    
+# Creates a new role_listing
+# Sample input:
+# {
+#     "Role_Listing_ID": 4,
+#     "Role_ID": 1,
+#     "Role_Listing_Desc": "This is a test role listing.",
+#     "Role_Listing_Source": 1,
+#     "Role_Listing_Close": "2021-01-01",
+#     "Role_Listing_Creator": 2
+# }
+@app.route("/role_listing", methods=['POST'])
+def create_role_listing():
+    data = request.get_json()
+    current_time = datetime.now()
+    current_date = current_time.date()
+    
+    role_listing = Role_Listing(data["Role_Listing_ID"], data["Role_ID"], data["Role_Listing_Desc"], data["Role_Listing_Source"], current_date, data["Role_Listing_Close"], data["Role_Listing_Creator"], func.now(), 0, '')
+    role_listing_check = Role_Listing.query.filter_by(Role_Listing_ID=data["Role_Listing_ID"]).first()
+    if role_listing_check:
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "role_listing": role_listing.json()
+                },
+                "message": "Role Listing already exists."
+            }
+        ), 400
+    else:
+        try:
+            db.session.add(role_listing)
+            db.session.commit()
+        except:
+            return jsonify(
+                {
+                    "code": 500,
+                    "data": {
+                        "role_listing": role_listing.json()
+                    },
+                    "message": "An error occurred creating the role_listing."
+                }
+            ), 500
+        return jsonify(
+            {
+                "code": 201,
+                "data": role_listing.json()
+            }
+        ), 201
+
+
+
+# Updates a role_listing
+# Sample input:
+# {
+#     "Role_Listing_ID": 4,
+#     "Role_Listing_Desc": "This is a test role listing.",
+#     "Role_Listing_Source": 1,
+#     "Role_Listing_Close": "2021-01-01",
+#     "Role_Listing_Updater": 2
+# }
+@app.route("/role_listing/update", methods=['PUT'])
+def update_role_listing():
+    data = request.get_json()
+    listing_id = data['Role_Listing_ID']
+    role_listing = Role_Listing.query.filter_by(Role_Listing_ID=listing_id).first()
+    if role_listing:
+        try:
+            role_listing.Role_Listing_Desc = data['Role_Listing_Desc']
+            role_listing.Role_Listing_Source = data['Role_Listing_Source']
+            role_listing.Role_Listing_Close = data['Role_Listing_Close']
+            role_listing.Role_Listing_Updater = data['Role_Listing_Updater']
+            role_listing.Role_Listing_ts_update = func.now()
+            db.session.commit()
+        except:
+            return jsonify(
+                {
+                    "code": 500,
+                    "data": {
+                        "role_listing": role_listing.json()
+                    },
+                    "message": "An error occurred updating the role_listing."
+                }
+            ), 500
+        return jsonify(
+            {
+                "code": 200,
+                "data": role_listing.json()
+            }
+        ), 200
+    else:
+        return jsonify(
+            {
+                "code": 404,
+                "data": {
+                    "listing_id": listing_id
+                },
+                "message": "Role Listing not found."
+            }
+        ), 404
+
 
 # Retrieves every role_listing in the database
 @app.route("/role_listing")
@@ -126,6 +229,55 @@ def find_by_role_id(role_id):
             "message": "Role Listing not found."
         }
     ), 404
+
+# Makes a staff member a candidate for a role listing
+# Sample Input:
+# {
+#     "candidate_id" : 1,
+#     "role_listing_id" : 1``
+# }
+@app.route("/role_listing/candidates", methods=['POST'])
+def create_candidate():
+    data = request.get_json()
+
+    candidate_id = data['candidate_id']
+    role_listing_id = data['role_listing_id']
+
+    if (Candidates.query.filter_by(Candidate_ID=candidate_id, Role_Listing_ID=role_listing_id).first()):
+        return jsonify(
+            {
+                "code": 400,
+                "data": {
+                    "candidate_id": candidate_id,
+                    "role_listing_id": role_listing_id
+                },
+                "message": "Candidate already exists."
+            }
+        ), 400
+
+    candidate = Candidates(candidate_id, role_listing_id)
+
+    try:
+        db.session.add(candidate)
+        db.session.commit()
+    except:
+        return jsonify(
+            {
+                "code": 500,
+                "data": {
+                    "candidate_id": candidate_id,
+                    "role_listing_id": role_listing_id
+                },
+                "message": "An error occurred creating the candidate."
+            }
+        ), 500
+
+    return jsonify(
+        {
+            "code": 201,
+            "data": candidate.json()
+        }
+    ), 201
 
 # Retrieves candidates based on role_listing_id
 @app.route("/role_listing/candidates/<int:role_listing_id>")
--- script.sql
+++ script.sql
@@ -231,16 +231,20 @@ Role_Listing_Open date not null,
 Role_Listing_Close date not null,
 Role_Listing_Creator int not null,
 Role_Listing_ts_create timestamp default current_timestamp,
-Country varchar(50) not null,
-Career_Level ENUM('Entry', 'Intermediate', 'First-level Mgmt', 'Middle-level Mgmt', 'Senior Mgmt') not null,
+-- Country varchar(50) not null,
+-- Career_Level ENUM('Entry', 'Intermediate', 'First-level Mgmt', 'Middle-level Mgmt', 'Senior Mgmt') not null,
 Role_Listing_Updater int,
 Role_Listing_ts_update timestamp default current_timestamp);
 
 -- insert data into role_listings table
 INSERT INTO role_listing_DB.role_listings VALUES
-(001, 002, 'Responsibilities for this role include:', 006, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 'Malaysia', 'Entry', 003, timestamp("2023-09-05")),
-(002, 014, 'Responsibilities for this role include:', 005, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 'Malaysia', 'Entry', 003, timestamp("2023-09-02")),
-(003, 015, 'Responsibilities for this role include:', 008, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 'Malaysia', 'Entry', 000, '');
+-- (001, 002, 'Responsibilities for this role include:', 006, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 'Malaysia', 'Entry', 003, timestamp("2023-09-05")),
+-- (002, 014, 'Responsibilities for this role include:', 005, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 'Malaysia', 'Entry', 003, timestamp("2023-09-02")),
+-- (003, 015, 'Responsibilities for this role include:', 008, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 'Malaysia', 'Entry', 000, '');
+
+(001, 002, 'Responsibilities for this role include:', 006, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 003, timestamp("2023-09-05")),
+(002, 014, 'Responsibilities for this role include:', 005, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 003, timestamp("2023-09-02")),
+(003, 015, 'Responsibilities for this role include:', 008, '2023-09-01', '2024-02-01', 003, timestamp("2023-09-01"), 000, '');
 
 -- create candidate table
 create table role_listing_DB.candidates
